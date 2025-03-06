77feea91a2d88e22bfd5d8d5a2fc75c5e73c3e20
lulojrz <lulojrz18@gmail.com>
2025-03-06 16:24:09 -0300

    arreglo de errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [ciudad,setCiudad] = useState("");
   const [dataclima, Setdataclima] = useState({})
+  const API_KEY = "9a03ba676c3d84cee2b1bfae6a8bcfd2";
   const url = `https://api.openweathermap.org/data/2.5/weather`
   const getWeatherData =  async (ciudad) => {
+     if(!ciudad){
+      alert("Ingrese una Ciudad")
+      return
+     }
+
+
     try {
-      const response = await fetch(`${url}?q=${ciudad}&appid=9a03ba676c3d84cee2b1bfae6a8bcfd2`)
+      const response = await fetch(`${url}?q=${ciudad}&appid=${API_KEY}`)
       const data =  await response.json();
       if (data.cod !== 200) {
         alert("Ciudad no encontrada");
@@ -31,6 +38,12 @@ function App() {
     getWeatherData(ciudad)
     
   }
+
+  
+
+  useEffect(() => {
+    getWeatherData("Madrid"); // Ciudad predeterminada
+  }, []);
   return (
     <div className="container">
       <h2 className="title">Weather APP</h2>
@@ -48,19 +61,25 @@ function App() {
           Search
         </button>
       </form>
-     {dataclima &&  ( 
-      <div className="card" style={{width:"50%", margin:"auto", marginTop : "30px"}}>
-      <div className="card-body">
-        <h5 className="card-title">{dataclima.name}({dataclima.sys.country})</h5>
-        <img src={`https://openweathermap.org/img/wn/${dataclima.weather[0].icon}@2x.png`} alt="" />
-        <h6 className="card-subtitle mb-2 text-body-secondary">Temperature</h6> 
-        <p className="card-text">{parseInt(dataclima.temp - 273.15)}°C</p>
-        
-      </div>
-    </div>
-     )}
+      {dataclima && dataclima.weather && (
+        <div className="card" style={{ width: "50%", margin: "auto", marginTop: "30px" }}>
+          <div className="card-body">
+            <h5 className="card-title">{dataclima.name}</h5>
+            <h6>{dataclima.sys.country}</h6>
+            <img
+              src={`https://openweathermap.org/img/wn/${dataclima.weather[0].icon}@2x.png`}
+              alt="Weather icon"
+            />
+            <h6 className="card-subtitle mb-2 text-body-secondary">Temperature</h6>
+            <p className="card-text">
+              {parseInt(dataclima.main.temp - 273.15)}°C
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
+
 
 export default App;
